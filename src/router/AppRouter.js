299ff3eb0cebfb48cee74f7f14b1ccadaa509eb3
dannohh <dtwomey13@gmail.com>
2022-05-10 18:44:25 -0500

    add the register form

--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -1,13 +1,20 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter, Routes, Route, useNavigate } from 'react-router-dom';
+
 import Header from '../components/Header';
 import AddMed from '../components/AddMed';
 import MedList from '../components/MedList';
 import useLocalStorage from '../hooks/useLocalStorage';
+import Register from '../components/Register';
 
 const AppRouter = () => {
 
+
+
   const [books, setBooks] = useLocalStorage('books', []);
+  const [isLoggedIn, setLogin] = useState('false')
+
+
 
   return (
     <BrowserRouter>
@@ -15,6 +22,7 @@ const AppRouter = () => {
         <Header />
         <div className="main-content">
           <Routes>
+            <Route element={<Register />} path="/register" exact={true} />
             <Route element={<MedList />} path="/" exact={true} />
             <Route path="/add" element={<AddMed books={books} setBooks={setBooks} />} />
           </Routes>
